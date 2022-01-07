872b87ce2ef67f250aa1aee6fe34a99d494c00f5
JorgeJC98 <69093957+JorgeJC98@users.noreply.github.com>
2022-01-07 13:26:43 -0600

    get post put delete methods

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -1,16 +1,53 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiAutores.Entidades;
 
 namespace WebApiAutores.Controllers {
     [ApiController]
     [Route("api/autores")]
     public class AutoresController: ControllerBase {
+
+        public readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context) {
+            this.context = context;
+        }
+
         [HttpGet]
-        public ActionResult<List<Autor>> Get() {
-            return new List<Autor>() {
-                new Autor() {Id=1, Nombre="jorge"},
-                new Autor() {Id=2, Nombre="Alberto"}
-            };
+        public async Task<ActionResult<List<Autor>>> Get() {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor) {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // api/autores/1 
+        public async Task<ActionResult> Put(Autor autor, int id) {
+            if( autor.Id != id ) {
+                return BadRequest("El id del autor no coincide con el id de la url");
+            }
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if( !existe ) {
+                return NotFound();
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] // api/autores/1 
+        public async Task<ActionResult> Delete(int id) {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if( !existe ) {
+                return NotFound();
+            }
+            context.Remove(new Autor() {Id = id});
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
